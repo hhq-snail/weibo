34a267d518002fcdb1065d5daadcafc6bd45389c
hhq <137383816@qq.com>
2023-04-24 06:20:23 +0000

    微博数据模型

--- app/Models/User.php
+++ app/Models/User.php
@@ -62,4 +62,9 @@ class User extends Authenticatable
         $hash = md5(strtolower(trim($this->attributes['email'])));
         return 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fsafe-img.xhscdn.com%2Fbw1%2F4893c456-4d98-47ca-a57a-34d28b068e01%3FimageView2%2F2%2Fw%2F1080%2Fformat%2Fjpg&refer=http%3A%2F%2Fsafe-img.xhscdn.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1684476717&t=0adf2505346fb50eee97549ec61312ed';
     }
+
+    public function statuses()
+    {
+        return $this->hasMany(status::class);
+    }
 }
